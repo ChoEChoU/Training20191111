2fc5e0f93f644b06f1a5034f9161977d8cc82604
Lim Mintaek <lmtna99@gmail.com>
2019-11-11 13:33:52 +0900

    MyHelloPanelListener 클래스 수정

--- MyHelloPanelListener.java
+++ MyHelloPanelListener.java
@@ -7,45 +7,63 @@ import javax.swing.*;
  * @author (작성자 이름) 
  * @version (버전번호나 날짜)
  */
-public class MyHelloPanelListener extends JPanel implements KeyListener, MouseListener
+public class MyHelloPanelListener extends JPanel
 {
     public JLabel ml;
     public MyHelloPanelListener(){
         ml = new JLabel("HELLO");
-        
         this.add(ml);
-        
+        this.addKeyListener(new myKeyListener(ml));
+        this.addMouseListener(new myMouseListener(ml));
     }
-    
-    public void keyPressed(KeyEvent e){
-    
+
+    class myMouseListener implements MouseListener{
+        public JLabel ml;
+        public myMouseListener(JLabel ml){
+            this.ml = ml;
+        }
+        public void mouseClicked(MouseEvent e){}
+
+        public void mouseEntered(MouseEvent e){}
+
+        public void mouseExited(MouseEvent e){}
+
+        public void mousePressed(MouseEvent e){
+            ml.setLocation(e.getX(), e.getY());
+        }
+
+        public void mouseReleased(MouseEvent e){
+
+        }
     }
-    
-    public void keyReleased(KeyEvent e){
-    
-    }
-    
-    public void keyTyped(KeyEvent e){
-    
-    }
-    
-    public void mouseClicked(MouseEvent e){
-    
-    }
-    
-    public void mouseEntered(MouseEvent e){
-    
-    }
-    
-    public void mouseExited(MouseEvent e){
-    
-    }
-    
-    public void mousePressed(MouseEvent e){
-    
-    }
-    
-    public void mouseReleased(MouseEvent e){
-    
+
+    class myKeyListener implements KeyListener{
+        public JLabel ml;
+        private final int FLYING_UNIT = 10;
+        public myKeyListener(JLabel ml){
+            this.ml = ml;
+        }
+        public void keyPressed(KeyEvent e){
+            int keyCode = e.getKeyCode();
+            int FLYING_UNIT = 10;
+            switch (keyCode) {
+                case KeyEvent.VK_UP:
+                ml.setLocation(ml.getX(), ml.getY()-FLYING_UNIT); break;
+                case KeyEvent.VK_DOWN:
+                ml.setLocation(ml.getX(), ml.getY()+FLYING_UNIT); break;
+                case KeyEvent.VK_LEFT:
+                ml.setLocation(ml.getX()-FLYING_UNIT, ml.getY()); break;
+                case KeyEvent.VK_RIGHT:
+                ml.setLocation(ml.getX()+FLYING_UNIT, ml.getY()); break;
+            }
+        }
+
+        public void keyReleased(KeyEvent e){
+
+        }
+
+        public void keyTyped(KeyEvent e){
+
+        }
     }
 }
