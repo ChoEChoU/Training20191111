c97a47b3e2c71255de3c19f18cb669e5c857e313
Lim Mintaek <lmtna99@gmail.com>
2019-11-11 13:03:32 +0900

    MyHelloFrame 중복 코드 삭제

--- MyHelloFrame.java
+++ MyHelloFrame.java
@@ -15,20 +15,12 @@ public class MyHelloFrame extends JFrame
     public MyHelloFrame(){
         this.setTitle("실습#3(2019.11.11)"); // 제목을 실습#3(2019.11.11)으로 설정
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //종료할때 시스템도 같이 종료
-       
-        this.setSize(300, 400); // 300, 400으로 크기 설정
-        this.setVisible(true);
-        mp = new JPanel();
-        this.add(mp);
         
         ml = new JLabel("HELLO");
         mp.add(ml);
         
         mp = new JPanel();
         this.add(mp);
-        
-        ml = new JLabel("HELLO");
-        mp.add(ml);
         
         this.setSize(300, 400);
         this.setVisible(true);
