5bc134ef6ad3044c89b696dc4dcc1049541650a9
Lim Mintaek <lmtna99@gmail.com>
2019-11-11 14:16:29 +0900

    MyHelloFrame 클래스 수정 MyHelloPanelListener에 Inner 클래스 없애고 다시 되돌린 후 코드 수정

--- MyHelloPanelListener.java
+++ MyHelloPanelListener.java
@@ -7,57 +7,53 @@ import javax.swing.*;
  * @author (2018315033 태영준, 2018315017 임민택) 
  * @version (2019.11.11)
  */
-public class MyHelloPanelListener extends JPanel
+public class MyHelloPanelListener extends JPanel implements MouseListener, KeyListener
 {
-    public JLabel ml;
+    public JLabel ml = new JLabel("HELLO");
+
     public MyHelloPanelListener(){
-        this.setLayout(null);
-        ml = new JLabel("HELLO");
-        ml.setSize(50,20);
-        ml.setLocation(30,30);
         this.add(ml);
-        this.addKeyListener(new myKeyListener(ml));
-        this.addMouseListener(new myMouseListener(ml));
-        ml.addKeyListener(new myKeyListener(ml));
     }
 
-    class myMouseListener implements MouseListener{
-        public JLabel ml;
-        public myMouseListener(JLabel ml){
-            this.ml = ml;
+    public void mouseClicked(MouseEvent e){
+    }
+
+    public void mouseEntered(MouseEvent e){   
+    }
+
+    public void mouseExited(MouseEvent e){   
+    }
+
+    public void mousePressed(MouseEvent e){
+        ml.setLocation(e.getX(),e.getY());
+    }
+
+    public void mouseReleased(MouseEvent e){   
+    }
+
+    public void keyPressed(KeyEvent e){
+        int x = ml.getX();
+        int y = ml.getY();
+        if ((e.getKeyCode()) == (e.VK_RIGHT)){
+            ml.setLocation(ml.getX()+10, ml.getY());
         }
-        public void mouseClicked(MouseEvent e){}
-
-        public void mouseEntered(MouseEvent e){}
-
-        public void mouseExited(MouseEvent e){}
-
-        public void mousePressed(MouseEvent e){
-            ml.setLocation(e.getX(), e.getY());
+        else if ((e.getKeyCode()) == (e.VK_DOWN)){
+            ml.setLocation(ml.getX(), ml.getY()+10);
         }
-
-        public void mouseReleased(MouseEvent e){
-
+        else if ((e.getKeyCode()) == (e.VK_LEFT)){
+            ml.setLocation(ml.getX()-10, ml.getY());
+        }
+        else if ((e.getKeyCode()) == (e.VK_UP)){
+            ml.setLocation(ml.getX(), ml.getY()-10);
         }
     }
 
-    class myKeyListener extends KeyAdapter{
-        public JLabel ml;
-        public myKeyListener(JLabel ml){
-            this.ml = ml;
-        }
-        public void keyPressed(KeyEvent e){
-            int keyCode = e.getKeyCode();
-            switch (keyCode) {
-                case KeyEvent.VK_UP:
-                    ml.setLocation(ml.getX(), ml.getY()-10); break;
-                case KeyEvent.VK_DOWN:
-                    ml.setLocation(ml.getX(), ml.getY()+10); break;
-                case KeyEvent.VK_LEFT:
-                    ml.setLocation(ml.getX()-10, ml.getY()); break;
-                case KeyEvent.VK_RIGHT:
-                    ml.setLocation(ml.getX()+10, ml.getY()); break;
-            }
-        }
+    public void keyReleased(KeyEvent e){
+    
+    }
+
+    public void keyTyped(KeyEvent e){
+    
     }
 }
+
